65bdd60034742bdba8aee4944f26d80f21406d2c
Jesus Guerrero <jesusant.guerrero@gmail.com>
2019-12-26 14:58:45 -0400

    git add mucklup crud

--- post/controllers/controllers.go
+++ post/controllers/controllers.go
@@ -3,17 +3,28 @@ package controllers
 import (
 	"net/http"
 
+	"github.com/jesusantguerrero/beeblog/post/models"
 	"github.com/labstack/echo"
 )
+
+var index = 1
+var posts []*models.Post
 
 func Routes(api *echo.Echo) *echo.Echo {
 
 	api.Add("GET", "/api/v1/posts", func(c echo.Context) error {
-		return c.String(http.StatusOK, "all the posts")
+		return c.JSON(http.StatusOK, posts)
 	})
 
 	api.Add("POST", "/api/v1/posts", func(c echo.Context) error {
-		return c.String(http.StatusCreated, "post created")
+		post := &models.Post{}
+		if err := c.Bind(post); err != nil {
+			return err
+		}
+		post.ID = index
+		index++
+		posts = append(posts, post)
+		return c.JSON(http.StatusCreated, post)
 	})
 
 	api.Add("DELETE", "/api/v1/posts", func(c echo.Context) error {
