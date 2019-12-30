96138b7e67a5500a97852239eafd844abd594db6
Jesus Guerrero <jesusant.guerrero@gmail.com>
2019-12-30 14:32:18 -0400

    refactor hello_test code

--- tdd/hello/hello_test.go
+++ tdd/hello/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestGreet(t *testing.T) {
-	got := Greet("go")
-	want := "Hello go"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf(" got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Greet user when the name is provided", func(t *testing.T) {
+		got := Greet("go")
+		want := "Hi, go.The world need your help"
+
+		assertMessage(t, got, want)
+	})
+
+	t.Run("Say bot name and ask for username when name is not provided", func(t *testing.T) {
+		got := Greet("")
+		want := "Hello my name is botcito. Can I know your name?"
+
+		assertMessage(t, got, want)
+	})
 }
